f00b25a9122463005f19287f5e799a8ea3048aa1
mhbalthasar <zhangfengwei@cafs.ac.cn>
2024-06-15 16:29:56 +0800

    反射器

---
+++ SerializationFormatters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalthasarIL
+{
+    public class SerializationFormatters
+    {
+        private Type? assemblyType = null;
+        private object instance = null;
+
+        private MethodInfo serialize;
+        private MethodInfo deserialize;
+        public SerializationFormatters(string DllPath= "BalthasarIL.BinaryFormatters.dll", string Formatter= "BinaryFormatter") 
+        {
+            var assembly = Assembly.LoadFrom(DllPath).GetTypes();
+            if(Formatter=="BinaryFormatter"){
+                //这只在传统的序列化器上使用，如果需要解锁任意类序列化，请使用BalthasarIL.BinaryFormatters.dll且不需要修改这里
+                var lacType = assembly.First(p => p.Name == "LocalAppContextSwitches");
+                var enField = lacType.GetField("s_binaryFormatterEnabled", BindingFlags.Static | BindingFlags.NonPublic);
+                enField.SetValue(null, 1);
+            }
+            this.assemblyType = assembly.First(p => p.Name == Formatter);
+            this.instance = this.assemblyType.GetConstructor(Type.EmptyTypes).Invoke(null);
+            this.serialize = this.assemblyType.GetMethod("Serialize");
+            this.deserialize = this.assemblyType.GetMethod("Deserialize");
+        }
+
+        public void Serialize(Stream stream,object obj)
+        {
+            this.serialize.Invoke(this.instance, new[] { stream,obj });
+        }
+
+        public object Deserialize(Stream stream) {
+            return this.deserialize.Invoke(this.instance, new[] { stream });
+        }
+    }
+}
